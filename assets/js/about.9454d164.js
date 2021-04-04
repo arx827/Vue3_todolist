84be7f73afd80a2e45d08d6db5785374d90f17b7
Eason Chen <eason.yc.chen@fubon.com>
2021-04-05 04:21:55 +0800

    deploy

---
+++ assets/js/about.9454d164.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var e=a("7a23"),o={class:"about"},c=Object(e.f)("h1",null,"This is an about page",-1);const s={render:function(n,t){return Object(e.o)(),Object(e.d)("div",o,[c])}};t.default=s}}]);
